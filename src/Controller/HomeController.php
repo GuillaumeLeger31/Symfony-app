b46538c4434c06845fa44e54578c701a2e1b3fdd
Guillaume Leger <guillaumeleger140@gmail.com>
2021-11-10 23:55:19 +0100

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Question;
+use App\Repository\QuestionRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(QuestionRepository $questionRepo): Response
+    {
+        $questions = $questionRepo->getQuestionsWithAuthors();
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'questions' => $questions
+        ]);
+    }
+}
